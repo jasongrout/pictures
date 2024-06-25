2a5738487eb72e22bbc6a6b0e16e261f89fea175
Jason Grout <jason@jasongrout.org>
2024-06-25 05:11:19 -0600

    Clean up names and update logic

--- picture.py
+++ picture.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Things we can still do:
 
 # [X] Turn off screen at night (see note below about `xset dpms force off`. Also see https://wiki.libsdl.org/FAQUsingSDL#Why_does_SDL_disable_my_screensaver_by_default.3F about allowing the screen to blank while running the program - we'll need to set an environment variable in this program using os.environ).
@@ -15,10 +16,23 @@
 # [ ] Handle pics with different sizes/orientations. Rotate? Put upright and blur the background? Center smaller pics
 # [ ] Hook up hardware buttons and switch: buttons to go prev/next, switch to hold a pic, buttons to rate a pic up or down.
 
+from random import SystemRandom
+import pygame
+import os
+import pygame.freetype
+import subprocess
+import datetime
+from bisect import bisect, bisect_left, bisect_right
+from pathlib import Path
+from collections import defaultdict
+from math import log
+import itertools
+from normdist import NormalDist
+
 # Set the variables so we can easily change the program
 FULLSCREEN = True
 SMALL_SCREEN_SIZE=(600,400)
-TIMER = 40 # minutes between picture changes
+DISPLAY_TIME_MS = 40 * 60 * 1000 # miliseconds a picture is displayed by default
 FONTSIZE = 120
 
 # The default text format:
@@ -28,7 +42,7 @@ FONTSIZE = 120
 FORMAT = 1
 
 # The directory of pictures
-PIC_DIRECTORY = '/home/pi/Export1080p/'
+PIC_DIRECTORY = Path('/home/pi/Export1080p/')
 
 # Time (hour, minute) of sleep and wake each day
 WAKE = (6, 30)
@@ -37,168 +51,113 @@ SLEEP = (21,30)
 
 
 # import stuff we need
-from random import SystemRandom
-import pygame
-import os
-import pygame.freetype
-import subprocess
-import re
-import datetime
-from bisect import bisect_left, bisect_right
 
-from time import process_time
-
-class timer:
-    def __enter__(self):
-        self.t = process_time()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.e = process_time()
-        self.elapsed = 1000*(self.e - self.t)
-        print(self)
-
-    def __float__(self):
-        return float(self.elapsed)
-
-    def __coerce__(self, other):
-        return (float(self), other)
-
-    def __str__(self):
-        return str(float(self))
-
-    def __repr__(self):
-        return str(float(self))
-
+################################################################
+## BEGIN X-based screen blanking
+###############################################################
 
 # Getting screen blanking to work well is a bit tricky.
 # 1. Edit /boot/config.txt and add 'hdmi_blanking=1' so that we can trigger the dpms off mode (see https://www.raspberrypi.org/documentation/configuration/config-txt/video.md and https://github.com/raspberrypi/linux/issues/487.)
 # 2. When this program is started, turn off the screen saver and dpms timeouts. Turn these back on when exiting. Note that if xscreensaver is installed, it already disables the default screensaver timeouts and sets the dpms timeouts and disables the dpms. However, when we force dpms off mode below, we activate dpms, which means that the existing timeouts then apply.
 
+# SCREENSAVER_SETTINGS = {}
+# def screensaver_off():
+#     global SCREENSAVER_SETTINGS
+#     xset = subprocess.check_output(['xset', '-q']).decode('utf-8')
+#     m=re.search(r'^\s*timeout:\s*(\d+)\s*cycle:\s*(\d+)', xset, re.MULTILINE)
+#     if m:
+#         SCREENSAVER_SETTINGS['timeout'] = int(m.groups()[0])
+#         SCREENSAVER_SETTINGS['cycle'] = int(m.groups()[1])
 
+#     m=re.search(r'^\s*Standby:\s*(\d+)\s*Suspend:\s*(\d+)\s*Off:\s*(\d+)', xset, re.MULTILINE)
+#     if m:
+#         SCREENSAVER_SETTINGS['standby'] = int(m.groups()[0])
+#         SCREENSAVER_SETTINGS['suspend'] = int(m.groups()[1])
+#         SCREENSAVER_SETTINGS['off'] = int(m.groups()[2])
+#     m = re.search(r'DPMS is (Enabled|Disabled)', xset)
+#     if m:
+#         setting = m.groups()[0]
+#         if setting == 'Enabled':
+#             SCREENSAVER_SETTINGS['enabled'] = True
+#         elif setting == 'Disabled':
+#             SCREENSAVER_SETTINGS['enabled'] = False
 
-SCREENSAVER_SETTINGS = {}
-def screensaver_off():
-    global SCREENSAVER_SETTINGS
-    xset = subprocess.check_output(['xset', '-q']).decode('utf-8')
-    m=re.search(r'^\s*timeout:\s*(\d+)\s*cycle:\s*(\d+)', xset, re.MULTILINE)
-    if m:
-        SCREENSAVER_SETTINGS['timeout'] = int(m.groups()[0])
-        SCREENSAVER_SETTINGS['cycle'] = int(m.groups()[1])
+#     subprocess.run('xset s 0 0 '.split())
+#     subprocess.run('xset dpms 0 0 0'.split())
 
-    m=re.search(r'^\s*Standby:\s*(\d+)\s*Suspend:\s*(\d+)\s*Off:\s*(\d+)', xset, re.MULTILINE)
-    if m:
-        SCREENSAVER_SETTINGS['standby'] = int(m.groups()[0])
-        SCREENSAVER_SETTINGS['suspend'] = int(m.groups()[1])
-        SCREENSAVER_SETTINGS['off'] = int(m.groups()[2])
-    m = re.search(r'DPMS is (Enabled|Disabled)', xset)
-    if m:
-        setting = m.groups()[0]
-        if setting == 'Enabled':
-            SCREENSAVER_SETTINGS['enabled'] = True
-        elif setting == 'Disabled':
-            SCREENSAVER_SETTINGS['enabled'] = False
-
-    subprocess.run('xset s 0 0 '.split())
-    subprocess.run('xset dpms 0 0 0'.split())
-
-def screensaver_restore():
-    global SCREENSAVER_SETTINGS
-    if 'timeout' in SCREENSAVER_SETTINGS:
-        subprocess.run('xset s {timeout} {cycle}'.format(**SCREENSAVER_SETTINGS).split())
-    if 'standby' in SCREENSAVER_SETTINGS:
-        subprocess.run('xset dpms {standby} {suspend} {off}'.format(**SCREENSAVER_SETTINGS).split())
-    if 'enabled' in SCREENSAVER_SETTINGS:
-        subprocess.run('xset {}dpms'.format('+' if SCREENSAVER_SETTINGS['enabled'] else '-').split())
+# def screensaver_restore():
+#     global SCREENSAVER_SETTINGS
+#     if 'timeout' in SCREENSAVER_SETTINGS:
+#         subprocess.run('xset s {timeout} {cycle}'.format(**SCREENSAVER_SETTINGS).split())
+#     if 'standby' in SCREENSAVER_SETTINGS:
+#         subprocess.run('xset dpms {standby} {suspend} {off}'.format(**SCREENSAVER_SETTINGS).split())
+#     if 'enabled' in SCREENSAVER_SETTINGS:
+#         subprocess.run('xset {}dpms'.format('+' if SCREENSAVER_SETTINGS['enabled'] else '-').split())
 
 # os.environ['SDL_VIDEO_ALLOW_SCREENSAVER']='1'
 # screensaver_off()
+# def display_sleep_x():
+#     """Turn off the display."""
+#     subprocess.run('xset dpms force off'.split())
+
+# def display_wake_x():
+#     """Turn on the display."""
+#     subprocess.run('xset dpms force on'.split())
+
+# def display_on_x():
+#     """Return True if the display is on, otherwise False."""
+#     output = subprocess.check_output('xset q'.split(), text=True)
+#     if 'Monitor is On' in output:
+#         return True
+#     elif 'Monitor is Off' in output:
+#         return False
+#     else:
+#         raise ValueError('Could not determine whether monitor was on or not')
+
+################################################################
+## END X-based screen blanking
+###############################################################
 
 
 def display_sleep():
     """Turn off the display."""
     # see https://forums.raspberrypi.com/viewtopic.php?t=363392 for more info
     os.system('ddcutil setvcp d6 4')
-    
-    # These did not work so well
-    # os.system('setterm -blank force')
-    # subprocess.run('setterm --blank 1', shell=True)
-    # subprocess.run('xset dpms force off'.split())
 
 def display_wake():
     """Turn on the display."""
     # see https://forums.raspberrypi.com/viewtopic.php?t=363392 for more info
     os.system('ddcutil setvcp d6 1')
-    # os.system('setterm -blank poke')
-    # subprocess.run('xset dpms force on'.split())
 
 def display_on():
     """Return True if the display is on, otherwise False."""
     # TODO: use pykms directly
     output = subprocess.check_output('ddcutil getvcp d6')
-    if '0x04' is in output:
+    if '0x04' in output:
         return True
-    elif '0x01' is in output:
+    elif '0x01' in output:
         return False
     else:
         raise ValueError('Could not determine whether monitor was on or not')
-        
-    # output = subprocess.check_output('xset q'.split(), text=True)
-    # if 'Monitor is On' in output:
-    #     return True
-    # elif 'Monitor is Off' in output:
-    #     return False
-    # else:
-    #     raise ValueError('Could not determine whether monitor was on or not')
 
-def date (filename):
+def format_filename(filename):
     """Format the filename string to display the date, depending on the global FORMAT."""
-    global FORMAT
     if FORMAT == 0:
         return ''
     elif FORMAT == 1:
         if filename[0] == '2':
-            return convert(filename)
+            return datetime.strptime(filename[:8], "%Y%m%d").strftime("%d %b %Y")
         else:
             return ''
     elif FORMAT == 2:
-        return filename[:-4]
+        os.path.splitext(filename)[0]
 
-def convert(date):
-    """Format a date string like 20200521 into a date for display like 21 May 2020."""
-    months = {
-        '01': 'Jan',
-        '02': 'Feb',
-        '03': 'Mar',
-        '04': 'Apr',
-        '05': 'May',
-        '06': 'June',
-        '07': 'July',
-        '08': 'Aug',
-        '09': 'Sep',
-        '10': 'Oct',
-        '11': 'Nov',
-        '12': 'Dec'
-    }
-    year = date[:4]
-    month = date[4:6]
-    day = date[6:8]
-    month = months[month]
-    return day+' '+month+' '+year
-
-def group(data):
-    """Group picture filenames like 20200528.jpg into days.
-
-    For now, we also only include filenames that match the current month.
-    """
-    dic = {}
+def group_by_day(data):
+    """Group picture filenames like 20200528.jpg into days."""
+    groups = defaultdict(list)
     for name in data:
-        start = name[:8]
-        if start not in dic:
-            dic[start] = [name]
-        else:
-            dic[start].append(name)
-    return dic
+        groups[name[:8]].append(name)
+    return groups
 
 # Start pygame up, setting allowed events
 pygame.freetype.init()
@@ -231,7 +190,7 @@ def show(filename = None):
         filename = CURRENT_PICTURE
         cimage = CURRENT_IMAGE
     else:
-        cimage = pygame.image.load(PIC_DIRECTORY + filename).convert()
+        cimage = pygame.image.load(PIC_DIRECTORY / filename).convert()
         CURRENT_PICTURE = filename
         CURRENT_IMAGE = cimage
 
@@ -247,9 +206,8 @@ def show(filename = None):
     else:
         pygame.transform.smoothscale(cimage, SMALL_SCREEN_SIZE, screen) # development
 
-
     # Put the filename 5 pixels from the bottom of the screen, 10 pixels from left edge
-    words = font.render( date(filename),  fgcolor=(255,255,255), bgcolor=(0,0,0,100))
+    words = font.render( format_filename(filename),  fgcolor=(255,255,255), bgcolor=(0,0,0,100))
     screen.blit(words[0], (10, screen_height - words[0].get_height() - 5))
 
     # Time is 5 pixels from bottom, 10 pixels from right edge
@@ -258,48 +216,43 @@ def show(filename = None):
 
     pygame.display.flip()
 
-
-# Total history of filenames that have been viewed, even across `seen` resets
-history = []
+# Total history of filenames that have been viewed since the program start, even across `seen` resets
+PIC_HISTORY = []
 
 # The position of the current picture in the history array
-position = -1
+PIC_HISTORY_INDEX = -1
 
 # Tracking data for the list of possible pictures, weights, etc.
-pic_files = []
-pic_dir_mtime = None
-groups = []
-days = []
-weights = []
-cumdist = []
+PIC_FILES = []
+PIC_DIRECTORY_MTIME = None
+PIC_GROUPS = []
+PIC_DAYS = []
+DAY_WEIGHTS = []
+CUM_DAY_WEIGHTS = []
 
 # The pics we have already seen in the current cycle of pictures. This can be reset
 # if we don't have many pictures left to show.
-seen = set()
+PICS_SEEN = set()
 
 # weight the days logarithmically
 # See https://docs.python.org/3.5/library/random.html#examples-and-recipes
-from math import log
-import itertools
-from bisect import bisect
-from normdist import NormalDist
 
 def reset_weights():
     """Reset the weights on the remaining pics in groups and days"""
-    global pic_dir_mtime, pic_files, weights, cumdist, groups, days
+    global PIC_DIRECTORY_MTIME, PIC_FILES, DAY_WEIGHTS, CUM_DAY_WEIGHTS, PIC_GROUPS, PIC_DAYS
 
     # If we have hardly any pics left (by weight), reset everything so the scan picks up everything
     # The threshold value relies on the log weighting scale and the kernel being normalized
-    if max(weights or [0])<=1e-5:
+    if max(DAY_WEIGHTS or [0])<=1e-5:
         seen = set()
-        pic_dir_mtime = None
+        PIC_DIRECTORY_MTIME = None
 
     # If the pic directory has changed (or mtime been reset), rescan all the files
-    if  pic_dir_mtime != os.path.getmtime(PIC_DIRECTORY):
-        pic_files = sorted(set(os.listdir(path=PIC_DIRECTORY)) - seen)
-        pic_dir_mtime = os.path.getmtime(PIC_DIRECTORY)
-        groups = group(pic_files)
-        days = list(groups.keys())
+    if  PIC_DIRECTORY_MTIME != PIC_DIRECTORY.stat().st_mtime:
+        PIC_FILES = sorted(set(os.listdir(PIC_DIRECTORY)) - seen)
+        PIC_DIRECTORY_MTIME = PIC_DIRECTORY.stat().st_mtime
+        PIC_GROUPS = group_by_day(PIC_FILES)
+        PIC_DAYS = list(PIC_GROUPS.keys())
 
     # Calculate the weights applied to each day based on how far their week
     # is from the current week
@@ -330,8 +283,8 @@ def reset_weights():
     # use log base N for weights, so one picture weights the day at 1.0,
     # N pictures makes a weight of 2, N^2 pictures makes a weight of 3,
     # N^3 pictures makes a weight of 4, and so on
-    weights = [(log(len(groups[day]), 2)+1)*day_weight(day) for day in days]
-    cumdist = list(itertools.accumulate(weights))
+    DAY_WEIGHTS = [(log(len(PIC_GROUPS[day]), 2)+1)*day_weight(day) for day in PIC_DAYS]
+    CUM_DAY_WEIGHTS = list(itertools.accumulate(DAY_WEIGHTS))
 
     #for (d,w) in sorted(zip(days, weights), key=lambda x: x[1]):
     #    print(d,"%f"%day_weight(d), "%f"%w)
@@ -374,69 +327,67 @@ while True:
             if random.random() < 0.1:
                 # Every tenth time or so, pick a picture from a random day, just to
                 # change things up a bit
-                day = random.choice(days)
+                day = random.choice(PIC_DAYS)
             else:
                 # pick a day, weighted according to cumdist, then pick a random pic from that day
                 # See https://docs.python.org/3.5/library/random.html#examples-and-recipes
-                day = days[bisect(cumdist, random.random() * cumdist[-1])]
-            x = random.choice(groups[day])
-            history.append(x)
-            seen.add(x)
+                day = PIC_DAYS[bisect(CUM_DAY_WEIGHTS, random.random() * CUM_DAY_WEIGHTS[-1])]
+            x = random.choice(PIC_GROUPS[day])
+            PIC_HISTORY.append(x)
+            PICS_SEEN.add(x)
+            PIC_HISTORY_INDEX = -1
 
-            position = -1
-            show(history[position])
+            show(PIC_HISTORY[PIC_HISTORY_INDEX])
 
             # Update the pics to remove the one we just showed so we don't show it again
-            groups[day].remove(x)
-            if len(groups[day])==0:
-                days.remove(day)
-                del groups[day]
+            PIC_GROUPS[day].remove(x)
+            if len(PIC_GROUPS[day])==0:
+                PIC_DAYS.remove(day)
+                del PIC_GROUPS[day]
             reset_weights()
 
-        pygame.time.set_timer(PICTURE_CHANGE,TIMER*60*1000)
+        pygame.time.set_timer(PICTURE_CHANGE,DISPLAY_TIME_MS)
 
 
     # Change the date format
     if (f.type == pygame.KEYDOWN and f.key == pygame.K_RETURN):
-        FORMAT = FORMAT+1
-        if FORMAT == 3:
-            FORMAT = 0
+        FORMAT = (FORMAT+1) % 3
         show()
 
     # Show the previous picture in history
     if ((f.type == pygame.KEYDOWN and f.key == pygame.K_LEFT)
         or (f.type == pygame.MOUSEBUTTONDOWN and f.button == 1)):
-        if len(history) > -position:
-            position -= 1
-            show(history[position])
-            pygame.time.set_timer(PICTURE_CHANGE,TIMER*60*1000)
+        if len(PIC_HISTORY) > -PIC_HISTORY_INDEX:
+            PIC_HISTORY_INDEX -= 1
+            show(PIC_HISTORY[PIC_HISTORY_INDEX])
+            pygame.time.set_timer(PICTURE_CHANGE,DISPLAY_TIME_MS)
 
     # Show the next picture in history
     if ((f.type == pygame.KEYDOWN and f.key == pygame.K_RIGHT)
         or (f.type == pygame.MOUSEBUTTONDOWN and f.button == 3)):
-        if position < -1:
-            position += 1
-            show(history[position])
-            pygame.time.set_timer(PICTURE_CHANGE,TIMER*60*1000)
+        if PIC_HISTORY_INDEX < -1:
+            PIC_HISTORY_INDEX += 1
+            show(PIC_HISTORY[PIC_HISTORY_INDEX])
+            pygame.time.set_timer(PICTURE_CHANGE,DISPLAY_TIME_MS)
 
     # Show the next picture in sorted order that we haven't seen yet (i.e., chronologically)
     # Do not put this pic in our history
     if f.type == pygame.KEYDOWN and f.key == pygame.K_DOWN:
         # Find the place just after where the current pic would have been
-        index = bisect_right(pic_files, CURRENT_PICTURE)
-        if index < len(pic_files):
-            show(pic_files[index])
-            pygame.time.set_timer(PICTURE_CHANGE,TIMER*60*1000)
+        index = bisect_right(PIC_FILES, CURRENT_PICTURE)
+        if index < len(PIC_FILES):
+            show(PIC_FILES[index])
+            pygame.time.set_timer(PICTURE_CHANGE,DISPLAY_TIME_MS)
 
     # Show the previous picture in sorted order that we haven't seen yet (i.e., chronologically)
     # Do not put this pic in our history
     if f.type == pygame.KEYDOWN and f.key == pygame.K_UP:
         # Find the place in the current pics array where we would have to insert the current pic
         # The pic we want is just before this.
-        index = bisect_left(pic_files, CURRENT_PICTURE) - 1
+        index = bisect_left(PIC_FILES, CURRENT_PICTURE) - 1
         if index > 0:
-            show(pic_files[index])
-            pygame.time.set_timer(PICTURE_CHANGE,TIMER*60*1000)
+            show(PIC_FILES[index])
+            pygame.time.set_timer(PICTURE_CHANGE,DISPLAY_TIME_MS)
 
     # Quit the program
     if f.type == pygame.QUIT:
